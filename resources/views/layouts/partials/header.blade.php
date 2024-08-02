5c1795bed6a9e5409208e99cfb3165f1dafc5699
Fernando Henrique <fernandohjesus777@gmail.com>
2024-08-02 11:08:57 -0300

    Alteracao na view novos produtos

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,3 +1,45 @@
+
+<div class="topbar">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="header-top-entry">
+
+                </div>
+
+            </div>
+            <div class="col-sm-8">
+                <ul class="top_link">
+                    <li>
+                        <a href="/minha-conta" class="uppercase">Minha conta
+                        </a>
+                    </li>
+                    <!-- Link de Login para Usuários Não Autenticados -->
+                    @if (!auth()->check())
+                        <li>
+                            <a href="/login" class="uppercase">Login</a>
+                        </li>
+                    @else
+                        <!-- Se o usuário ESTIVER autenticado -->
+                        <li>
+                            <a href="/checkout/create" class="uppercase">checkout</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('logout') }}" class="uppercase"
+                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                Sair
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </li>
+                    @endif
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 <header>
     <nav class="navbar navbar-default navbar-sticky bootsnav">
         <div class="container">
